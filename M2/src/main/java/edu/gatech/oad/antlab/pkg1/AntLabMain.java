9e1afc5960cdc3c3d5e8d164121a9d3be2c87aa7
Timothy Lee-O'Connor <tleeoconnor@yahoo.com>
2015-05-27 19:53:58 -0400

    Fixed the GT ID Requirement
    Changed buzzcard to GT ID

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -74,7 +74,7 @@ import edu.gatech.oad.antlab.pkg2.*;
                   //Person4 replace P4 with your name
                   //and gburdell4 with your gt id
                   Person4 p4 = new Person4("Timothy Lee-O'Connor");
-                  toPrint += p4.toString("903115715");
+                  toPrint += p4.toString("tjlo3");
                   
                   //Person5 replace P5 with your name
                   //and gburdell5 with your gt id
